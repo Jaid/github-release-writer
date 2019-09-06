d818c7cfa3cfa9f9d0389307b2bbb426ee6e2b25
Jaid <jaid.jsx@gmail.com>
2019-09-07 01:02:21 +0200

    fix: Fixed initial release handling
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/lib/generateChangelog/index.js
+++ src/lib/generateChangelog/index.js
@@ -60,66 +60,68 @@ function getCommitTypeByPrefix(prefix) {
 export default options => {
   const authors = {}
   const commitCategories = {}
-  for (const {author, commit, sha} of options.comparison.commits) {
-    commit.sha = sha
-    commit.shortSha = sha.substring(0, 6)
-    const authorId = String(author.id)
-    if (authors.hasOwnProperty(authorId)) {
-      authors[authorId].commits++
-    } else {
-      authors[authorId] = {
-        avatar: author.avatar_url,
-        gravatarId: author.gravatar_id,
-        url: author.html_url,
-        name: author.login,
-        commits: 1,
-      }
-    }
-    commit.authorData = authors[authorId]
-    const regex = / *(?<prefix>\w+) *: *(?<message>.+)/s
-    const parsed = regex.exec(commit.message)
-    if (parsed) {
-      commit.originalMessage = commit.message
-      const {prefix} = parsed.groups
-      const commitType = getCommitTypeByPrefix(prefix)
-      commit.prefix = prefix.toLowerCase()
-      if (commitType) {
-        commit.message = parsed.groups.message
-        const commitTypeId = camelCase(commitType.title)
-        if (!commitCategories.hasOwnProperty(commitTypeId)) {
-          commitCategories[commitTypeId] = {
-            commits: [],
-            id: commitTypeId,
-            title: commitType.title,
-            emoji: commitType.emoji,
-          }
-        }
-        commitCategories[commitTypeId].commits.push(commit)
-      }
-      commit.message = /.*/.exec(commit.message)[0].trim()
-    }
-  }
   const dependencies = {}
-  for (const [dependencyType, {title}] of Object.entries(dependencyTypes)) {
-    const events = options.dependencyChanges[dependencyType]
-    if (!events) {
-      continue
+  if (!options.isInitialRelease) {
+    for (const {author, commit, sha} of options.comparison.commits) {
+      commit.sha = sha
+      commit.shortSha = sha.substring(0, 6)
+      const authorId = String(author.id)
+      if (authors.hasOwnProperty(authorId)) {
+        authors[authorId].commits++
+      } else {
+        authors[authorId] = {
+          avatar: author.avatar_url,
+          gravatarId: author.gravatar_id,
+          url: author.html_url,
+          name: author.login,
+          commits: 1,
+        }
+      }
+      commit.authorData = authors[authorId]
+      const regex = / *(?<prefix>\w+) *: *(?<message>.+)/s
+      const parsed = regex.exec(commit.message)
+      if (parsed) {
+        commit.originalMessage = commit.message
+        const {prefix} = parsed.groups
+        const commitType = getCommitTypeByPrefix(prefix)
+        commit.prefix = prefix.toLowerCase()
+        if (commitType) {
+          commit.message = parsed.groups.message
+          const commitTypeId = camelCase(commitType.title)
+          if (!commitCategories.hasOwnProperty(commitTypeId)) {
+            commitCategories[commitTypeId] = {
+              commits: [],
+              id: commitTypeId,
+              title: commitType.title,
+              emoji: commitType.emoji,
+            }
+          }
+          commitCategories[commitTypeId].commits.push(commit)
+        }
+        commit.message = /.*/.exec(commit.message)[0].trim()
+      }
     }
-    const hasChanges = Object.values(events).some(hasContent)
-    if (!hasChanges) {
-      continue
-    }
-    logger.debug("Found changes for \"%s\"", title)
-    dependencies[dependencyType] = {title}
-    for (const [eventType, eventList] of Object.entries(events)) {
-      if (hasContent(eventList)) {
-        if (eventType === "moved") {
-          dependencies[dependencyType][eventType] = eventList.map(entry => ({
-            ...entry,
-            oldTypesString: humanizeList(entry.oldTypes),
-          }))
-        } else {
-          dependencies[dependencyType][eventType] = eventList
+    for (const [dependencyType, {title}] of Object.entries(dependencyTypes)) {
+      const events = options.dependencyChanges[dependencyType]
+      if (!events) {
+        continue
+      }
+      const hasChanges = Object.values(events).some(hasContent)
+      if (!hasChanges) {
+        continue
+      }
+      logger.debug("Found changes for \"%s\"", title)
+      dependencies[dependencyType] = {title}
+      for (const [eventType, eventList] of Object.entries(events)) {
+        if (hasContent(eventList)) {
+          if (eventType === "moved") {
+            dependencies[dependencyType][eventType] = eventList.map(entry => ({
+              ...entry,
+              oldTypesString: humanizeList(entry.oldTypes),
+            }))
+          } else {
+            dependencies[dependencyType][eventType] = eventList
+          }
         }
       }
     }
