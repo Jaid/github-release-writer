b61a5cb192a63071f840ea5e75dcba0db03bf812
Jaid <jaid.jsx@gmail.com>
2021-05-10 22:08:35 +0200

    Fixed Probot integration
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/plugins/githubApi/index.js
+++ src/plugins/githubApi/index.js
@@ -23,16 +23,16 @@ export default class GithubApi {
   async init() {
     const privateKey = await fsp.readFile(config.pemFilePath, "utf8")
     const server = new Server({
+      port: config.webhookPort,
       Probot: Probot.defaults({
         privateKey,
         secret: config.webhookSecret,
         appId: config.githubAppId,
-        port: config.webhookPort,
       }),
     })
-    logger.info("GitHub app %s is listening to webhook port %s", config.githubAppId, config.webhookPort)
     await server.load(probotApp)
     server.start()
+    logger.info("GitHub app %s is listening to webhook port %s", config.githubAppId, config.webhookPort)
   }
 
 }
