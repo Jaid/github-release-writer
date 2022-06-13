81878a8d133ffaa0d5fdbfcc573dc7460f0d481b
Jaid <jaid.jsx@gmail.com>
2022-06-13 21:37:46 +0200

    Did some experimental Webpack config changes
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,30 @@ import configure from "webpack-config-jaid"
  * @type {import("webpack").Configuration}
  */
 const extra = {
+  output: {
+    asyncChunks: true,
+    chunkFormat: "module",
+    chunkLoading: "import",
+    clean: true,
+    filename: "[name].js",
+    chunkFilename: "[id].js",
+  },
+  optimization: {
+    removeAvailableModules: true,
+    runtimeChunk: {
+      name: "runtime",
+    },
+    splitChunks: {
+      chunks: "async",
+      cacheGroups: {
+        defaultVendors: {
+          test: /[/\\]node_modules[/\\]/,
+          name: "vendor",
+          chunks: "all",
+        },
+      },
+    },
+  },
   module: {
     parser: {
       javascript: {
